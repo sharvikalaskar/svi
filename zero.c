45cef3839e768cf7e279963c9e814a65333d7f76
Sharvi Kalaskar <sharvikalaskar268@gmail.com>
2026-09-06 20:40:26 +0530

    Add files via upload

---
+++ zero.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void)
+{
+    int num;
+    printf("Enter the number: \n");
+    scanf("%d", &num);
+    if (num == 0)
+    printf("Given number is zero");
+    else
+    printf("Given number is not zero");
+}
